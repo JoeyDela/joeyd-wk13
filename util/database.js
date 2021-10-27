58f974dac60f32c256e391eff17e30fff1e6053c
JoeyDela <duffy.west87@gmail.com>
2021-10-27 17:47:56 -0500

    modles

--- util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize("sequelize", "root", "password", {
-    dialect: "mysql",
-    host: "localhost"
-});
-
-module.exports = sequelize;
